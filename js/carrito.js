3f6f387389a15bf77a14b462eff91b5a03571e23
Gabriel Hertzan <gabriel.hertzan@gmail.com>
2025-05-02 11:45:38 -0300

    commit para grar rama

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,6 @@
 function iconoCarrito() {
     const cantidadCarrito = document.getElementById("cantidadArticulos");
     const carritoStorage = sessionStorage.getItem("carrito");
-    const cantidadCarritoStorage = sessionStorage.getItem("cantidadCarrito");
     const btnCarrito = document.getElementById("btnCarrito");
 
     if (carritoStorage) {
@@ -15,13 +14,35 @@ function iconoCarrito() {
                 footer: '<a href="#">Why do I have this issue?</a>'
               });
         };
+    }else{
+        btnCarrito.disable = true;
     }
+}
+
+function toCurrency(num){
+    return new Intl.NumberFormat("es-AR", {style: "currency", currency: "ARS"}).format(num);
+}
+
+function deleteItem(carrito, item){
+    return carrito.filter(e => e !== item);
+}
+
+function btnDeleteFromCart(carrito){
+    const botones = document.querySelectorAll(".btn-delete-from-cart");
+
+    botones.forEach(boton => {
+        boton.onclick = (e) =>{
+            const id = e.currentTarget.id;
+            carrito = deleteItem(carrito, carrito.find(e => e.item.id == id));
+            sessionStorage.setItem("carrito", JSON.stringify(carrito));
+        };
+    });
 }
 
 function getCarritoHtml(carrito){
     let div = document.createElement("div");
     let tabla = document.createElement("table");
-    tabla.className = "table";
+    tabla.className = "table gh-table";
     let thead = document.createElement("thead");
     let tbody = document.createElement("tbody");
 
@@ -41,10 +62,14 @@ function getCarritoHtml(carrito){
         tr.innerHTML= `
             <th scope="row">${carrito.indexOf(e) + 1}</th>
             <td>${e.item.descripcion}</td>
-            <td>${e.cantidad}</td>
-            <td>${e.item.precio}</td>
-            <td>${e.item.precio * e.cantidad}</td>
-            <td><a id="eliminarDelCarrito" class="btn"> X </a></td>
+            <td>
+                <button class="btn btn-light btn-sm restar" id="restar-${e.item.id}"> <span> < </span></button>
+                <span class="cantidad" id="cantidad-${e.item.id}"> 1 </span>
+                <button class="btn btn-light btn-sm sumar" id="sumar-${e.item.id}"> <span> > </span></button>
+            </td>
+            <td>${toCurrency(e.item.precio)}</td>
+            <td>${toCurrency(e.item.precio * e.cantidad)}</td>
+            <td><a class="btn btn-delete-from-cart" id=${e.item.id}> X </a></td>
         `;
 
         tbody.appendChild(tr);
@@ -56,3 +81,4 @@ function getCarritoHtml(carrito){
 
     return div.innerHTML;
 }
+
