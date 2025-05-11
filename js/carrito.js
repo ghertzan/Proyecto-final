cb84229c11cf3eedb8c33d318a92871427a5e97e
Gabriel Hertzan <gabriel.hertzan@gmail.com>
2025-05-11 19:57:58 -0300

    Finalizado

--- js/carrito.js
+++ js/carrito.js
@@ -1,66 +1,78 @@
-function clearCartView(){
-    const cartContainerDiv = document.getElementById("containerCarrito");
-    cartContainerDiv.innerHTML = "";
+function clearCartView() {
+  const cartContainerDiv = document.getElementById("containerCarrito");
+  cartContainerDiv.innerHTML = "";
 }
 
-function createCartView(){
-    clearCartView();
-    iconoCarrito();
-    const cartContainerDiv = document.getElementById("containerCarrito");
-    const cart = getCart();
-    if(!cart || cart.length == 0){
-        Swal.fire({
-            title: "Tu compra...",
-            text: "El carrito está vacío...",
-            icon: "question"
-          });
-    }else{
-        cart.forEach(e => {
-            const cartItemDiv = document.createElement("div");
-            cartItemDiv.className = "cart-item";
-            cartItemDiv.innerHTML = `
+function createCartView() {
+  clearCartView();
+  iconoCarrito();
+  const cartContainerDiv = document.getElementById("containerCarrito");
+  const cart = getCart();
+  if (!cart || cart.length == 0) {
+    Swal.fire({
+      title: "Tu compra...",
+      text: "El carrito está vacío...",
+      icon: "question",
+      confirmButtonText: " OK",
+    }).then((result) => {
+      window.location.assign("./productos.html");
+    });
+  } else {
+    cart.forEach((e) => {
+      const cartItemDiv = document.createElement("div");
+      cartItemDiv.className = "cart-item";
+      cartItemDiv.innerHTML = `
                 <img src=${e.item.img}>
                 <h5 class="texto-carrito">${e.item.descripcion}</h5>
                 <div>
                     <button class="btn btn-sm resta"> < </button>
-                    <span class="cantidad-item-carrito texto-carrito"> ${e.cantidad} </span>
+                    <span class="cantidad-item-carrito texto-carrito"> ${
+                      e.cantidad
+                    } </span>
                     <button class="btn btn-sm suma"> > </button>
                 </div>
-                <h5 class="texto-carrito">Precio: ${toCurrency(e.item.precio)}</h5>
-                <h5 class="texto-carrito">Total: ${toCurrency(e.cantidad * e.item.precio)}</h5>
+                <h5 class="texto-carrito">Precio: ${toCurrency(
+                  e.item.precio
+                )}</h5>
+                <h5 class="texto-carrito">Total: ${toCurrency(
+                  e.cantidad * e.item.precio
+                )}</h5>
             `;
-            cartContainerDiv.appendChild(cartItemDiv);
-            const btnRestar = cartItemDiv.querySelector(".resta");
-            const btnSumar = cartItemDiv.querySelector(".suma");
-            btnSumar.onclick = () => {
-                addToCart(e.item, 1);
-                createCartView();     
-            };
-            btnRestar.onclick = () => {
-                removeFromCart(e.item, 1);
-                createCartView();
-            }
-        });
-        const footerDiv = document.createElement("div");
-        footerDiv.className = "cart-footer";
-        footerDiv.innerHTML = `
-            <h5 class="texto-carrito">Total del carrito: ${toCurrency(cart.reduce((acc, curr) => acc + (curr.item.precio * curr.cantidad), 0))} </h5>
+      cartContainerDiv.appendChild(cartItemDiv);
+      const btnRestar = cartItemDiv.querySelector(".resta");
+      const btnSumar = cartItemDiv.querySelector(".suma");
+      btnSumar.onclick = () => {
+        addToCart(e.item, 1);
+        createCartView();
+      };
+      btnRestar.onclick = () => {
+        removeFromCart(e.item, 1);
+        createCartView();
+      };
+    });
+    const footerDiv = document.createElement("div");
+    footerDiv.className = "cart-footer";
+    footerDiv.innerHTML = `
+            <h5 class="texto-carrito">Total del carrito: ${toCurrency(
+              cart.reduce(
+                (acc, curr) => acc + curr.item.precio * curr.cantidad,
+                0
+              )
+            )} </h5>
             <button class="btn comprar mt-3"> Comprar</button>
             <button class="btn vaciar-carrito mt-3"> Vaciar carrito</button>
             `;
-        cartContainerDiv.appendChild(footerDiv); 
-        const bntVaciar = footerDiv.querySelector(".vaciar-carrito");
-        const btnComprar = footerDiv.querySelector(".comprar");
-        bntVaciar.onclick = () =>{
-            emptyCart();
-            createCartView();
-        };
-        btnComprar.onclick = () => {
-            createCheckoutView(cart);
-        };
-    }
+    cartContainerDiv.appendChild(footerDiv);
+    const bntVaciar = footerDiv.querySelector(".vaciar-carrito");
+    const btnComprar = footerDiv.querySelector(".comprar");
+    bntVaciar.onclick = () => {
+      emptyCart();
+      createCartView();
+    };
+    btnComprar.onclick = () => {
+      createCheckoutView(cart);
+    };
+  }
 }
-
-
 
 createCartView();
--- js/checkoutView.js
+++ js/checkoutView.js
@@ -12,54 +12,33 @@ function createCheckoutView(cart) {
   cartResumeDiv.className = "col-md-5 col-lg-4 order-md-last";
 
   paymentMethodDiv.innerHTML = `
-        <form class="needs-validation text-light" id="checkout-form">
+        <form class="needs-validation text-light" id="checkout-form" type="submit">
           <div class="row g-3">
             <div class="col-sm-6">
               <label for="firstName" class="form-label">Nombre</label>
               <input type="text" class="form-control" id="firstName" name="firstName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Se requiere un nombre válido.
-              </div>
             </div>
 
             <div class="col-sm-6">
               <label for="lastName" class="form-label">Apellido</label>
               <input type="text" class="form-control" id="lastName" name="lastName" placeholder="" value="" required>
-              <div class="invalid-feedback">
-                Se requiere apellido válido.
-              </div>
             </div>
 
             <div class="col-12">
               <label for="email" class="form-label">Email</label>
-              <input type="email" class="form-control" id="email" name="email" placeholder="tu@example.com" required             <div class="invalid-feedback">
-                Ingresa una dirección de correo electrónico válida para actualizaciones de envío.
-              </div>
+              <input type="email" class="form-control" id="email" name="email" placeholder="tu@example.com" required>             
             </div>
 
             <div class="col-12">
               <label for="address" class="form-label">Dirección</label>
               <input type="text" class="form-control" id="address" name="address" placeholder="" required>
-              <div class="invalid-feedback">
-                Por favor introduce tu direccion de envio.
-                Please enter your shipping address.
-              </div>
-            </div>
-
-            <div class="col-12">
-              <label for="address2" class="form-label">Dirección 2 (opcional)</label>
-              <input type="text" class="form-control" id="address2" name="address2" placeholder="Piso/Depto">
             </div>
 
             <div class="col-md-3">
               <label for="zip" class="form-label">Código postal</label>
               <input type="text" class="form-control" id="zip" name="zip" placeholder="" required>
-              <div class="invalid-feedback">
-                Código postal requerido.
-              </div>
             </div>
           </div>
-
           <hr class="my-4">
 
           
@@ -74,43 +53,27 @@ function createCheckoutView(cart) {
               <input id="debit" name="paymentMethodCC" type="radio" class="form-check-input" value="DC" required>
               <label class="form-check-label" for="debit">Tarjeta de débito</label>
             </div>
-            <div class="form-check">
-              <input id="transfer" name="paymentMethodCC" type="radio" class="form-check-input" value="TR" required>
-              <label class="form-check-label" for="transfer">Transferencia</label>
-            </div>
           </div>
 
           <div class="row gy-3">
             <div class="col-md-6">
               <label for="cc-name" class="form-label">Nombre en la tarjeta</label>
-              <input type="text" class="form-control" id="cc-name" name="cc-name" placeholder="Tal como figura en la tarjeta" required>
-              <div class="invalid-feedback">
-                Se requiere el nombre en la tarjeta
-              </div>
+              <input type="text" class="form-control" id="cc-name" name="ccName" placeholder="Tal como figura en la tarjeta" required>
             </div>
 
             <div class="col-md-6">
               <label for="cc-number" class="form-label">Número de tarjeta de crédito</label>
-              <input type="text" class="form-control" id="cc-number" name="cc-number" placeholder="" required>
-              <div class="invalid-feedback">
-                Se requiere número de tarjeta de crédito
-              </div>
+              <input type="text" class="form-control" id="cc-number" name="ccNumber" placeholder="" required>
             </div>
 
             <div class="col-md-3">
               <label for="cc-expiration" class="form-label">Vencimiento</label>
-              <input type="text" class="form-control" id="cc-expiration" name="cc-expiration" placeholder="" required>
-              <div class="invalid-feedback">
-                Fecha de vencimiento requerida
-              </div>
+              <input type="text" class="form-control" id="cc-expiration" name="ccExpiration" placeholder="" required>
             </div>
 
             <div class="col-md-3">
               <label for="cc-cvv" class="form-label">CVV</label>
-              <input type="text" class="form-control" id="cc-cvv" name="cc-cvv" placeholder="" required>
-              <div class="invalid-feedback">
-                Código de seguridad requerido
-              </div>
+              <input type="text" class="form-control" id="cc-cvv" name="ccCvv" placeholder="" required>
             </div>
           </div>
 
@@ -120,6 +83,9 @@ function createCheckoutView(cart) {
         </form>
     `;
 
+  /**
+   * A partir de aquí se crea el resumen del carrito para la confirmación de la compra
+   */
   const h4 = document.createElement("h4");
   h4.className = "d-flex justify-content-between align-items-center mb-3";
   h4.innerHTML = `
@@ -164,11 +130,32 @@ function createCheckoutView(cart) {
   divCheckout.append(paymentMethodDiv, cartResumeDiv);
   cartContainerDiv.appendChild(divCheckout);
 
-  document.getElementById("checkout-btn").onclick = () => {
-    const datosCliente = getFormData(cart);
+  /**
+   * Acciones luego de enviar el formulario
+   */
+  const form = document.getElementById("checkout-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    data.cart = cart;
+    console.log(data);
     Swal.fire({
       title: "Resumen de la compra...",
       text: "Si presionas OK la compra se procesará.",
+      html: `
+        <p>Total de la compra: ${toCurrency(
+          cart.reduce(
+            (acc, curr) => (acc += curr.cantidad * curr.item.precio),
+            0
+          )
+        )}</p>
+        <p>Pagado con ${
+          data.paymentMethodCC == "CC"
+            ? "Tarjeta de Crédito"
+            : "Tarjeta de Dédito"
+        } número ${data.ccNumber}</p>  
+      `,
       showCancelButton: true,
       confirmButtonText: "Si, Lo quiero",
       showClass: {
@@ -185,25 +172,23 @@ function createCheckoutView(cart) {
       animate__faster
     `,
       },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Confirmado!",
+          html: `<p>Tu pedido es el #${Math.floor(
+            Math.random() * 100000 - 1
+          )}, te enviaremos un correo a: ${
+            data.email
+          }, con toda la información.</p>
+          <p>Muchas Gracias por tu compra.</p>`,
+          icon: "success",
+          confirmButtonText: "OK",
+        }).then((result) => {
+          emptyCart();
+          window.location.assign("./productos.html");
+        });
+      }
     });
-  };
-}
-
-function getFormData(cart) {
-  const paymentRecord = {
-    nombre: document.querySelector("#firstName").value,
-    apellido: document.querySelector("#lastName").value,
-    email: document.querySelector("#email").value,
-    direccion: document.querySelector("#address").value,
-    direccion2: document.querySelector("#address2").value,
-    codigoPostal: document.querySelector("#zip").value,
-    payMethod: document.querySelector("[name=paymentMethodCC]:checked").value,
-    ccNombre: document.querySelector("#cc-name").value,
-    ccNumero: document.querySelector("#cc-number").value,
-    ccVencimiento: document.querySelector("#cc-expiration").value,
-    ccCvv: document.querySelector("#cc-cvv").value,
-    cart: cart,
-  };
-
-  return paymentRecord;
+  });
 }
